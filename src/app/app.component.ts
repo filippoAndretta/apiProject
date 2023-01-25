a784d74cd51ac57f5241418f86479856087f8220
Filippo Andretta <filippo.andretta@bluewin.ch>
2023-01-25 19:12:56 +0100

    added api's calls added subscription to load the data created 3 tables with 4 rows to display the data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {LoadingServiceService} from "./loading-service.service";
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ApiProject';
+  busData: any;
+  busWData: any;
+  stationData: any;
+  constructor(private loadingServiceService: LoadingServiceService) { }
+
+  ngOnInit(){
+    this.loadingServiceService.getBusData().subscribe(res => {
+      this.busData = res;
+    })
+
+    this.loadingServiceService.getTrainData().subscribe(res => {
+      this.busWData = res;
+    })
+
+    this.loadingServiceService.getStationData().subscribe(res => {
+      this.stationData = res;
+    })
+  }
 }
